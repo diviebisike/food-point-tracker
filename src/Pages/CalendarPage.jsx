d442945f5ce69089030eff21167ce1b5a7c5f43b
Angelica Deribeaux <ad583@duke.edu>
2025-07-13 19:52:17 -0400

    style updates but not much work done

--- src/Pages/CalendarPage.jsx
+++ src/Pages/CalendarPage.jsx
@@ -3,8 +3,8 @@ import ReactCalendar from '../Components/Calendar';
 export function CalendarPage(){
     return(
     <>
-    <h1>Calendar</h1>
-    <p>Choose the dates you will not be at Duke:</p>
+    <h1 className = "top-banner">Calendar</h1>
+    <p className = "top-banner">Choose the dates you will not be at Duke:</p>
       <div>
         <ReactCalendar/>
       </div>
