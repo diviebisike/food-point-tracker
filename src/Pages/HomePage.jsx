5c06004db14510be9d8b35771444445542669fd3
Erica Zhang <eyz7@duke.edu>
2025-07-21 23:09:29 -0400

    Switched order of calendar and tracking

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,24 +1,28 @@
-import SimpleCalendar from "../Components/BasicCal"
-import '../SimpCal.css'
-import { StatsPic } from "../Components/StatsPic"
+import SimpleCalendar from "../Components/BasicCal";
+import "../SimpCal.css";
+import { StatsPic } from "../Components/StatsPic";
 
-export function HomePage(){
+export function HomePage() {
+  return (
+    <>
+      <div className="top-banner-home">
+        <h1 className="duke_title">Duke</h1>
+        <h3 className="dining_title">DINING BUDGET TOOL</h3>
+      </div>
 
-    return(
-        <>
-        <div className = "top-banner-home">
-            <h1 className = "duke_title"> Duke </h1>
-            <h3 className = "dining_title"> DINING BUDGET TOOL</h3>
-        </div>
-        <h3 className = "basic">Calendar Integration</h3>
-        <div className = "simp-cal-wrapper">
-        <SimpleCalendar/>
-        </div>
-        <hr/>
-        <h3 className = "basic"> Tracking & Alerts</h3>
-        <StatsPic />
-        <hr/>
-        <h3 className = "basic"> Planning Tools</h3>
-        </>
-    )
+      <h3 className="basic">Tracking & Alerts</h3>
+      <StatsPic />
+
+      <hr />
+
+      <h3 className="basic">Calendar Integration</h3>
+      <div className="simp-cal-wrapper">
+        <SimpleCalendar />
+      </div>
+
+      <hr />
+
+      <h3 className="basic">Planning Tools</h3>
+    </>
+  );
 }
